570edb235c1365420ca623921d657e293ebc511e
Dat Pham <datpuk@gmail.com>
2022-01-05 17:11:04 +0000

    dynamic programming for q3.2020

--- British-Informatics-Olympiad/2020/q3.cpp
+++ British-Informatics-Olympiad/2020/q3.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int p, q, r;
-long n;
+long long n, permutationCount;
 string s;
-long permutationCount;
+
+map<pair<int, int>, long long> sections;
 
 void generatePermutation(int idx, int sameCount) {
 	if (permutationCount >= n) return;
@@ -15,31 +17,35 @@ void generatePermutation(int idx, int sameCount) {
 		if (permutationCount == n) cout << s << endl;
 		return;
 	}
+	
+	auto pastKey = make_pair(idx, sameCount);
+	if (sections.count(pastKey) && permutationCount + sections[pastKey] < n) {
+		permutationCount += sections[pastKey];
+		return;
+	}
+	long long oldPermutationCount = permutationCount;
 	for (int i = 0; i < p; i++) {
-		if (idx == 0) {
+		if (idx == 0 || 'A' + i != s[idx - 1]) {
 			s[idx] = 'A' + i;
 			generatePermutation(idx + 1, 1);
 		}
-		else if ('A' + i == s[idx-1] && sameCount < q) {
+		else if ('A' + i == s[idx - 1] && sameCount < q) {
 			s[idx] = 'A' + i;
-			generatePermutation(idx + 1, sameCount+1);
-		}
-		else if ('A' + i != s[idx - 1]) {
-			s[idx] = 'A' + i;
-			generatePermutation(idx + 1, 1);
+			generatePermutation(idx + 1, sameCount + 1);
 		}
 	}
+	long long sectionCount = permutationCount - oldPermutationCount;
+	sections[pastKey] = sectionCount;
 }
 
-//9 / 27 marks
 int main() {
 	//while (true) {
 		cin >> p >> q >> r >> n;
 		string temp(r, ' ');
 		s = temp;
 		permutationCount = 0;
+		sections.clear();
 		generatePermutation(0, 0);
 	//}
-
 	return 0;
 }
